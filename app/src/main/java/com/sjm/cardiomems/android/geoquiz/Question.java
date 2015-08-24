5cd41c60f753599458eba975de98a59d850dec24
Shawn Veader <shawn@veader.org>
2015-08-24 13:41:43 -0700

    Chapter 2: Finished

---
+++ app/src/main/java/com/sjm/cardiomems/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.sjm.cardiomems.android.geoquiz;
+
+/**
+ * Created by sveader on 8/24/15.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
